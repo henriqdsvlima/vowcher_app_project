2c98ca13b9a9ad14dbbb45f9a974fe51b1d78d56
Henrique <61509462+hmriq@users.noreply.github.com>
2021-05-15 14:33:29 -0300

    refatorando o codigo

--- lib/main.dart
+++ lib/main.dart
@@ -1,118 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:vowcher_app_project/screens/vowcher_login.dart';
 
 void main() {
   runApp(VowcherLogin());
 }
 
-class VowcherLogin extends StatefulWidget {
-  @override
-  _VowcherLoginState createState() => _VowcherLoginState();
-}
-
-class _VowcherLoginState extends State<VowcherLogin> {
+class VowcherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final logo = Hero(
-      tag: 'hero',
-      child: CircleAvatar(
-        backgroundColor: Colors.transparent,
-        child: Text(
-          ' Vowcher',
-          style: TextStyle(
-            fontSize: 60,
-            color: Color(0xffffffff),
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        radius: 48.0,
-      ),
-    );
-
-    final email = TextFormField(
-      cursorColor: Color(0xffff5d53),
-      keyboardType: TextInputType.emailAddress,
-      autofocus: false,
-      initialValue: '',
-      decoration: InputDecoration(
-        hintText: ' Insira o Email',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(32.0),
-        ),
-      ),
-    );
-
-    final password = TextFormField(
-      autofocus: false,
-      obscureText: true,
-      initialValue: '',
-      decoration: InputDecoration(
-        hintText: 'Password',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(32.0),
-        ),
-      ),
-    );
-
-    final loginButton = Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
-      child: RaisedButton(
-        onPressed: () {},
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(24),
-        ),
-        padding: EdgeInsets.all(12),
-        color: Colors.red,
-        child: Text(
-          'Log In',
-          style: TextStyle(color: Colors.white, fontSize: 12.0),
-        ),
-      ),
-    );
-
-    final forgotlabel = TextButton(
-      autofocus: true,
-      onPressed: () {},
-      child: Container(
-        child: Text(
-          'Esqueceu sua senha?',
-          style: TextStyle(color: Colors.white, fontSize: 10.0),
-        ),
-      ),
-    );
-
-    final Register = ElevatedButton(
-      autofocus: false,
-      onPressed: () {},
-    );
-
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color(0xffff5d53),
-        body: Center(
-          child: ListView(
-            shrinkWrap: true,
-            padding: EdgeInsets.only(left: 24.0, right: 24.0),
-            children: <Widget>[
-              logo,
-              SizedBox(
-                height: 48.0,
-              ),
-              email,
-              SizedBox(
-                height: 8.0,
-              ),
-              password,
-              SizedBox(
-                height: 24.0,
-              ),
-              loginButton,
-              forgotlabel,
-            ],
-          ),
-        ),
-      ),
+      home: VowcherLogin(),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:vowcher_app_project/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(VowcherLogin());
+    await tester.pumpWidget(VowcherApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
