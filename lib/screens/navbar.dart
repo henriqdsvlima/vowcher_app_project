2e0aa372e5b883e2129ada7e34e70b1849f04f47
Henrique <61509462+hmriq@users.noreply.github.com>
2021-05-29 16:11:40 -0300

    novas telas e configurações da pagina

--- lib/screens/navbar.dart
+++ lib/screens/navbar.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'cupom_home.dart';
 import 'profile.dart';
-import 'vowcher_login.dart';
-import 'vowcher_register.dart';
 
 class NavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 3,
+        length: 2,
         child: Scaffold(
           appBar: PreferredSize(
             preferredSize: Size.fromHeight(60.0),
@@ -30,9 +29,8 @@ class NavBar extends StatelessWidget {
             ),
           ),
           body: TabBarView(children: <Widget>[
-            VowcherLogin(),
-            VowcherRegister(),
             VowcherProfile(),
+            CupomHomePage(),
           ]),
         ));
   }
