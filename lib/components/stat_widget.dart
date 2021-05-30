0bd31c8c186c7c967aa4aa8097fc338a80a1dac8
SidneyRJ <sidney.requenajr@hotmail.com>
2021-05-29 23:29:15 -0300

    Página de Info

---
+++ lib/components/stat_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+Widget statWidget(String title, String stat) {
+  return Expanded(
+    child: Column(
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18.0,
+          ),
+        ),
+        Text(
+          stat,
+          style: TextStyle(
+            fontSize: 16.0,
+          ),
+        ),
+      ],
+    ),
+  );
+}
