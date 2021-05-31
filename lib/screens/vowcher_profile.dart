95f92ff496be8e06751cf3237ef592f6837ccea8
SidneyRJ <sidney.requenajr@hotmail.com>
2021-05-31 15:36:00 -0300

    ScrollView Profile

--- lib/screens/vowcher_profile.dart
+++ lib/screens/vowcher_profile.dart
@@ -9,47 +9,49 @@ class VowcherProfile extends StatelessWidget {
     return Scaffold(
       body: Container(
         width: double.infinity,
-        child: Column(
-          children: [
-            Container(
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: CircleAvatar(
-                      radius: 30,
-                      child: Align(
-                        alignment: Alignment.bottomRight,
-                        child: Avatar(),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: CircleAvatar(
+                        radius: 30,
+                        child: Align(
+                          alignment: Alignment.bottomRight,
+                          child: Avatar(),
+                        ),
                       ),
                     ),
-                  ),
-                  Text(
-                    'Olá,\n $_nomeUsuario',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+                    Text(
+                      'Olá,\n $_nomeUsuario',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Divider(),
-            Container(
-              child: Card(
-                elevation: 10,
-                child: Center(
-                  heightFactor: 20.0,
-                  child: Text(
-                    '$_nomeUsuario \nSão Paulo-SP',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
+              Divider(),
+              Container(
+                child: Card(
+                  elevation: 10,
+                  child: Center(
+                    heightFactor: 20.0,
+                    child: Text(
+                      '$_nomeUsuario \nSão Paulo-SP',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Divider(),
-          ],
+              Divider(),
+            ],
+          ),
         ),
       ),
     );
